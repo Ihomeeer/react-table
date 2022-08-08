1f8b039dc91eecf1e16d9619abb8ba07f6c07b3f
Ihomeeer <ihomeer@gmail.com>
2022-08-08 17:16:52 +0300

    fixed styles in modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,8 @@
 // компонент карточки
+import React from 'react';
 import styles from './Modal.module.css';
+import cx from 'classnames';
+import iconRatingPath from '../../vendor/icons/rating.svg';
 
 const Modal = (props) => {
 
@@ -31,10 +34,21 @@ const Modal = (props) => {
             <img src={props.data.images[0]} alt={props.data.title} className={styles.cardImage} />
 
             <article className={styles.cardArticle}>
-              <p className={styles.cardText}>{`Brand: ${props.data.brand}`}</p>
-              <p className={styles.cardText}>{`Price: ${props.data.price}`}</p>
-              <p className={styles.cardText}>{`Rating: ${props.data.rating}`}</p>
-              <p className={styles.cardText}>{props.data.description}</p>
+
+              <div className={styles.titleContainer}>
+              <p className={cx(styles.cardText, styles.cardTitle)}>{props.data.brand}</p>
+              <p className={styles.category}>{props.data.category}</p>
+              </div>
+
+
+
+              <div className={styles.ratingContainer}>
+                <p className={styles.cardText}>{props.data.rating}</p>
+                <img src={iconRatingPath} className={styles.ratingIcon} alt="rating" />
+                <p className={cx(styles.cardText, styles.price)}>${props.data.price}</p>
+              </div>
+
+              <p className={cx(styles.cardText, styles.description)}>{props.data.description}</p>
             </article>
             <button className={styles.cardCopyButton} onClick={handleCopyData}>Copy data</button>
           </div>
